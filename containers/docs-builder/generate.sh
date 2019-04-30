b2d199e99fdcdd506eb58bbeebe0ce5448a36de6
Kirill Makhonin <kroks.rus@gmail.com>
2019-04-30 10:22:41 +0300

    [#52] Add pdf generation

--- containers/docs-builder/generate.sh
+++ containers/docs-builder/generate.sh
@@ -4,5 +4,5 @@ SRC_DIR=source
 OUT_DIR=out
 
 sphinx-build -b html $SRC_DIR $OUT_DIR
-#sphinx-build -b rinoh $SRC_DIR $OUT_DIR-pdf
+sphinx-build -b rinoh $SRC_DIR $OUT_DIR-pdf
 #find $OUT_DIR -type f -name '*.html' | xargs sed  -i'' '/href="\./s/\.md/\.html/'
